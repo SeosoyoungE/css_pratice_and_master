db69d575345096f6d72d7c34860cc1a864cf4d1c
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-22 07:29:19 +0000

    모바일 네비바 추가

--- dist/index.0fe47027.js
+++ dist/index.0fe47027.js
@@ -1,9 +1,18 @@
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButton = document.querySelectorAll(".plan button");
+const modalNegativeBtn = document.querySelector(".modal__action--negative");
+//클릭시 모달창 띄우기
 for(let i = 0; i < selectPlanButton.length; i++)selectPlanButton[i].addEventListener("click", ()=>{
     backdrop.classList.remove("hidden");
     modal.classList.remove("hidden");
 });
+//모달 No버튼 클릭시 모달창 없애기
+const closeClick = ()=>{
+    backdrop.classList.add("hidden");
+    modal.classList.add("hidden");
+};
+modalNegativeBtn.addEventListener("click", closeClick);
+modal.addEventListener("click", closeClick);
 
 //# sourceMappingURL=index.0fe47027.js.map
