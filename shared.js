5f72789f55dd5fcaa088a782c63f81e6503c8bb1
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-28 02:02:12 +0000

    responsive web: add meta-viewport

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButton = document.querySelectorAll(".plan button");
 const modalNegativeBtn = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
 //클릭시 모달창 띄우기
 for (let i = 0; i < selectPlanButton.length; i++) {
@@ -11,10 +13,21 @@ for (let i = 0; i < selectPlanButton.length; i++) {
   });
 }
 
-//모달 No버튼 클릭시 모달창 없애기
-const closeClick = () => {
+//모달 No버튼 클릭시 모달창 없애기 & 모바일nav 없애기
+const closeModal = () => {
   backdrop.classList.add("hidden");
-  modal.classList.add("hidden");
+  if (modal) {
+    modalNegativeBtn.addEventListener("click", closeModal);
+  }
+  mobileNav.classList.add("hidden");
 };
-modalNegativeBtn.addEventListener("click", closeClick);
-modal.addEventListener("click", closeClick);
+if (modalNegativeBtn) {
+  modalNegativeBtn.addEventListener("click", closeModal);
+}
+backdrop.addEventListener("click", closeModal);
+
+//햄버거 버튼 클릭시 모바일nav 뜨기
+toggleButton.addEventListener("click", () => {
+  mobileNav.classList.remove("hidden");
+  backdrop.classList.remove("hidden");
+});
